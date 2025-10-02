32864a101298b96af78db003b8eec3773e446fd1
SR <saodat.riskiyeva@gmail.com>
2025-10-01 22:54:18 -0400

    adding user signup

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
-db.connectToDatabase().then(function () {
-  app.listen(3000);
-}).catch(function (error) {
-  console.log("Failed to connect to the database!");
-  console.log(error);
-});
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to the database!");
+    console.log(error);
+  });
