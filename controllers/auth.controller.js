a3f9efcbd5eb98ed5d0f32f410a0a842e19586f7
SR <saodat.riskiyeva@gmail.com>
2025-10-06 07:06:14 -0400

    displaying error message and saving user input

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,12 +4,27 @@ const validation = require("../util/validation");
 const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res) {
-  res.render("customer/auth/signup");
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      confirmEmail: "",
+      password: "",
+      fullname: "",
+      street: "",
+      postal: "",
+      city: "",
+    };
+  }
+
+  res.render("customer/auth/signup", { inputData: sessionData });
 }
 
 async function signup(req, res, next) {
-  const eneteredData = {
+  const enteredData = {
     email: req.body.email,
+    confirmEmail: req.body["confirm-email"],
     password: req.body.password,
     fullname: req.body.fullname,
     street: req.body.street,
@@ -31,8 +46,9 @@ async function signup(req, res, next) {
     sessionFlash.flashDataToSession(
       req,
       {
-        errorMessage: "Please check your input.",
-        ...eneteredData,
+        errorMessage:
+          "Please check your input. Password must be at least 6 character slong, postal code must be 5 characters long.",
+        ...enteredData,
       },
       function () {
         res.redirect("/signup");
@@ -57,8 +73,8 @@ async function signup(req, res, next) {
       sessionFlash.flashDataToSession(
         req,
         {
-          errorMessage: "User exists already.",
-          ...eneteredData,
+          errorMessage: "User exists already! Try logging in instead!",
+          ...enteredData,
         },
         function () {
           res.redirect("/signup");
@@ -77,13 +93,21 @@ async function signup(req, res, next) {
 }
 
 function getLogin(req, res) {
-  res.render("customer/auth/login");
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      password: "",
+    };
+  }
+
+  res.render("customer/auth/login", { inputData: sessionData });
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
   let existingUser;
-
   try {
     existingUser = await user.getUserWithSameEmail();
   } catch (error) {
@@ -92,7 +116,8 @@ async function login(req, res) {
   }
 
   const sessionErrorData = {
-    errorMessage: "Invalid credentials - please check your email and password.",
+    errorMessage:
+      "Invalid credentials - please double-check your email and password!",
     email: user.email,
     password: user.password,
   };
