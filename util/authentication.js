dc6d4274faac3a3f35683f042df6e56f33457e2e
SR <saodat.riskiyeva@gmail.com>
2025-10-07 07:15:57 -0400

    adding product admin pages

--- util/authentication.js
+++ util/authentication.js
@@ -1,5 +1,5 @@
 function createUserSession(req, user, action) {
-  req.session.uid = user._id.toStrong();
+  req.session.uid = user._id.toString();
   req.session.isAdmin = user.isAdmin;
   req.session.save(action);
 }
