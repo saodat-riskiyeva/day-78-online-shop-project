6fd6e2d09086a567bc4f107f10e93cb3d2c3c1e3
SR <saodat.riskiyeva@gmail.com>
2025-10-07 07:51:50 -0400

    adding a product model and storing products in the database

---
+++ models/product.model.js
@@ -0,0 +1,27 @@
+const db = require("../data/database");
+
+class Product {
+  constructor(productData) {
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = +productData.price;
+    this.description = productData.description;
+    this.image = productData.image; // the name of the image file
+    this.imagePath = `product-data/images/${productData.imagePath}`; // the path to the image file
+    this.imageUrl = `/products/assets/images/${productData.imagePath}`; // the URL to access the image
+  }
+
+  async save() {
+    const productData = {
+      title: this.title,
+      summary: this.summary,
+      price: this.price,
+      description: this.description,
+      image: this.image,
+    };
+
+    await db.getDb().collection("products").insertOne(productData);
+  }
+}
+
+module.exports = Product;
